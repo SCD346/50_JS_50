44a3baa8a7afbc4147f37725ed0da88be58ffb0d
steve_D <scd346@nyu.edu>
2021-04-14 12:26:37 -0700

    modal update

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const modalTarget = document.querySelector('.page-wrapper');
-const modalCloser = document.querySelector('.info-modal');
+const modalCloser = document.querySelector('.close');
+const modalContent = document.querySelector('.modal')
 
 
 
 window.addEventListener('load', (event) => {
     modalTarget.classList.add('page-dim');
+    modalContent.classList.add('info-modal-shown');
   });
 
-window.addEventListener('click', (event)=> {
+modalCloser.addEventListener('click', (event)=> {
     modalTarget.classList.remove('page-dim');
+    modalContent.classList.remove('info-modal-shown')
 });
