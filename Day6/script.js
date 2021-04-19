3c22775f2ff1a555b6089597d0d0957cd12ae038
steve_D <scd346@nyu.edu>
2021-04-19 09:41:07 -0700

    box animation function implemented - Works

--- Day6/script.js
+++ Day6/script.js
@@ -5,5 +5,15 @@ window.addEventListener('scroll', checkBoxes)
 checkBoxes()
 
 function checkBoxes() {
-    console.log(window.innerHeight)
+    const triggerBottom = window.innerHeight / 5 * 4
+
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
+
+        if(boxTop < triggerBottom) {
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show')
+        }
+    })
 }
