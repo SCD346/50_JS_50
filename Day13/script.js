0ca849f248a7c23ebdc41e9f0d6eb3bebe2aab5e
steve_D <scd346@nyu.edu>
2021-04-25 18:39:11 -0700

    filters and trims for white space

--- Day13/script.js
+++ Day13/script.js
@@ -8,5 +8,9 @@ textarea.addEventListener('keyup', (e) => {
 })
 
 function createTags(input) {
-    console.log(input)
+    const tags = input.split(',').filter(tag => tag.trim()
+    !== '').map(tag => tag.trim())
+    console.log(tags)
 }
+
+
