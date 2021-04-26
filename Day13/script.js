0071565fa79da998fc8bb44624bf2088b3a5a7b3
steve_D <scd346@nyu.edu>
2021-04-25 18:41:49 -0700

    tags render under textarea

--- Day13/script.js
+++ Day13/script.js
@@ -10,7 +10,14 @@ textarea.addEventListener('keyup', (e) => {
 function createTags(input) {
     const tags = input.split(',').filter(tag => tag.trim()
     !== '').map(tag => tag.trim())
-    console.log(tags)
+    tagsEl.innerHTML = ''
+
+    tags.forEach(tag => {
+        const tagEl = document.createElement('span')
+        tagEl.classList.add('tag')
+        tagEl.innerText = tag
+        tagsEl.appendChild(tagEl)
+    });
 }
 
 
