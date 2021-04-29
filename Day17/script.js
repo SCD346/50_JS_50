3539725270e1d7b4f32313b185123783403100cb
steve_D <scd346@nyu.edu>
2021-04-29 15:12:28 -0700

    search term results log to console - Works

--- Day17/script.js
+++ Day17/script.js
@@ -1,6 +1,9 @@
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=c74068788fbc8ed9b09205823b49f82d&page-1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=c74068788fbc8ed9b09205823b49f82d&query="'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=c74068788fbc8ed9b09205823b49f82d&query="'
+
+const form = document.getElementById('form')
+const search = document.getElementById('search')
 
 //Get initial movies
 getMovies(API_URL)
@@ -11,3 +14,17 @@ async function getMovies(url) {
 
     console.log(data.results)
 }
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const searchTerm = search.value
+
+    if(searchTerm && searchTerm !== '') {
+        getMovies(SEARCH_API + searchTerm)
+
+        search.value = ''
+    } else {
+        window.location.reload()
+    }
+})
