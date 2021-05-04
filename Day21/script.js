dd131519312508e32472d94c32e0f77c87d7bb4d
steve_D <scd346@nyu.edu>
2021-05-04 08:25:33 -0700

    listeners added

--- Day21/script.js
+++ Day21/script.js
@@ -1,3 +1,8 @@
+const fill = document.querySelector('.fill')
+const empties = document.querySelectorAll('.empty')
+
+fill.addEventListener('dragstart', dragStart)
+fill.addEventListener('dragend', dragEnd)
 
 
 
