f9f9fb3313b3d6c57cd58c4d8e0bd7a3560906f5
steve_D <scd346@nyu.edu>
2021-05-04 08:23:04 -0700

    fn set up

---
+++ Day21/script.js
@@ -0,0 +1,27 @@
+
+
+
+function dragStart() {
+    console.log('drag start')
+}
+
+
+function dragEnd() {
+    console.log('drag end')
+}
+
+function dragOver() {
+    console.log('drag over')
+}
+
+function dragEnter() {
+    console.log('drag enter')
+}
+
+function dragLeave() {
+    console.log('drag leave')
+}
+
+function dragDrop() {
+    console.log('drag drop')
+}
