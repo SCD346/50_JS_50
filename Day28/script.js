b21386d947feb9f37110c52720780704e22778a7
steve_D <scd346@nyu.edu>
2021-05-15 19:54:46 -0600

    repos render to card

--- Day28/script.js
+++ Day28/script.js
@@ -1,8 +1,9 @@
+//mine
 const APIURL = 'https://api.github.com/users/'
 
+const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
-const main = document.getElementById('main')
 
 async function getUser(username) {
     try {
@@ -10,10 +11,21 @@ async function getUser(username) {
 
         // console.log(data)
         createUserCard(data)
+        getRepos(username)
     } catch(err) {
-        if(err.response.status === 404) {
+        if(err.response.status == 404) {
             createErrorCard('No user found.')
         }
+    }
+}
+
+async function getRepos(username) {
+    try {
+        const { data } = await axios(APIURL + username + '/repos?sort=created')
+
+        addReposToCard(data)
+    } catch(err) {
+            createErrorCard('Problem fetching repos')
     }
 }
 
@@ -32,25 +44,38 @@ function createUserCard(user) {
                     <li>${user.followers} <strong>Followers</strong></li>
                     <li>${user.following} <strong>Following</strong></li>
                     <li>${user.public_repos} <strong>Repos</strong></li>
-
+                </ul>
 
                     <div id="repos"></div>
-
-                </ul>
             </div>
         </div>
     `
     main.innerHTML = cardHTML
 }
 
-function createErrorCard(msg){
+function createErrorCard(msg) {
     const cardHTML = `
     <div class="card">
         <h1>${msg}</h1>
     </div>
     `
-
     main.innerHTML = cardHTML
+}
+
+function addReposToCard(repos) {
+    const reposEl = document.getElementById('repos')
+
+    repos
+        .forEach(repo => {
+            const repoEl = document.createElement('a')
+            repoEl.classList.add('repo')
+            repoEl.href = repo.html_url
+            repoEl.target = '_blank'
+            repoEl.innerText = repo.name
+
+            reposEl.appendChild(repoEl)
+
+        })
 }
 
 form.addEventListener('submit', (e) => {
