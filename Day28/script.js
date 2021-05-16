f46e9b50b2223702c65597a2f099b29f673725a7
steve_D <scd346@nyu.edu>
2021-05-15 19:30:04 -0600

    template literal set up

--- Day28/script.js
+++ Day28/script.js
@@ -2,18 +2,48 @@ const APIURL = 'https://api.github.com/users/'
 
 const form = document.getElementById('form')
 const search = document.getElementById('search')
+const main = document.getElementById('main')
 
 async function getUser(username) {
     try {
         const { data } = await axios(APIURL + username)
 
-        console.log(data)
+        // console.log(data)
+        createUserCard(data)
     } catch(err) {
         console.log(err)
     }
 }
 
+function createUserCard(user) {
+    const cardHTML = `
+    <div class="card">
+            <div>
+                <img src="https://randomuser.me/api/portraits/men/30.jpg" alt="" class="avatar">
+            </div>
 
+            <div class="user-info">
+                <h2>John Doe</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus, qui?</p>
+
+                <ul>
+                    <li>300 <strong>Followers</strong></li>
+                    <li>400 <strong>Following</strong></li>
+                    <li>30 <strong>Repos</strong></li>
+
+
+                    <div id="repos">
+                        <a href="#" class="repo">Repo 1</a>
+                        <a href="#" class="repo">Repo 2</a>
+                        <a href="#" class="repo">Repo 3</a>
+                    </div>
+
+                </ul>
+            </div>
+        </div>
+    `
+    main.innerHTML = cardHTML
+}
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
