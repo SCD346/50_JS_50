fa68aeb1452682cf6d3a5d0d40bfd217eef792f1
steve_D <scd346@nyu.edu>
2021-05-21 12:47:56 -0700

    randomizing functions created

---
+++ Day31/script.js
@@ -0,0 +1,21 @@
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+}
+
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+}
+
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+}
+
+function getRandomSymbol() {
+    const symbols = '!@#$%^&*(){}+<>?/,.'
+    return symbols [Math.floor(Math.random() * symbols.length)]
+}
+
+console.log(getRandomLower())
+console.log(getRandomUpper())
+console.log(getRandomNumber())
+console.log(getRandomSymbol())
