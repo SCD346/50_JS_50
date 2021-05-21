6816aa17d2227338c6e2ce741393fca98fe8c5d1
steve_D <scd346@nyu.edu>
2021-05-21 13:15:03 -0700

    pw gen fn done - Works

--- Day31/script.js
+++ Day31/script.js
@@ -30,7 +30,19 @@ function generatePassword(lower, upper, number, symbol, length) {
     const typesCount = lower + upper + number + symbol
     const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0])
 
-    console.log(typesArr)
+    if(typesCount === 0) {
+        return ''
+    }
+
+    for(let i = 0; i < length; i+=typesCount) {
+        typesArr.forEach(type => {
+            const funcName = Object.keys(type)[0]
+            generatedPassword += randomFunc[funcName]()
+        })
+    }
+
+    const finalPassword = generatedPassword.slice(0, length)
+    return finalPassword
 }
 
 
