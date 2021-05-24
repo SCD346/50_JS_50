dc7d42965c95512fff303134f420616a2b5e1748
steve_D <scd346@nyu.edu>
2021-05-24 10:45:54 -0700

    notes in local storage will render

--- Day33/script.js
+++ Day33/script.js
@@ -1,5 +1,11 @@
 const addBtn = document.getElementById('add')
 
+const notes = JSON.parse(localStorage.getItem('notes'))
+
+if(notes) {
+    notes.forEach(note => addNewNote(note))
+}
+
 addBtn.addEventListener('click', () => addNewNote())
 
 function addNewNote(text = '') {
