649b252556d07ac36a8b90be10dc401d0dd02f4e
steve_D <scd346@nyu.edu>
2021-06-07 13:03:30 -0400

    types and colors - Works

--- Day37/script.js
+++ Day37/script.js
@@ -17,6 +17,8 @@ const colors = {
 	normal: '#F5F5F5'
 }
 
+const mainTypes = Object.keys(colors)
+
 const fetchPokemons = async () => {
     for(let i = 1; i <= pokemon_count; i++) {
         await getPokemon(i)
@@ -34,9 +36,29 @@ const createPokemonCard = (pokemon) => {
     const pokemonEl = document.createElement('div')
     pokemonEl.classList.add('pokemon')
 
-    const pokemonInnerHTML = `
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
+    const id = pokemon.id.toString().padStart(3, '0')
 
+    const poke_types = pokemon.types.map(type => type.type.name)
+    const type = mainTypes.find(type => poke_types.indexOf(type) > -1 )
+    const color = colors[type]
+
+    pokemonEl.style.backgroundColor = color
+
+    const pokemonInnerHTML = `
+    <div class="img-container">
+        <img src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" alt="">
+    </div>
+    <div class="info">
+        <span class="number">${id}</span>
+        <h3 class="name">${name}</h3>
+        <small class="type">Type: <span>${type}</span> </small>
+    </div>
     `
+
+    pokemonEl.innerHTML = pokemonInnerHTML
+
+    pokeContainer.appendChild(pokemonEl)
 }
 
 fetchPokemons()
