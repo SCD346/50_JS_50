ac76478c126dcfb65e1964f56d2a4ea84605fa70
steve_D <scd346@nyu.edu>
2021-06-09 18:34:03 -0400

    api renders, live search results

--- Day42/script.js
+++ Day42/script.js
@@ -4,13 +4,15 @@ const listItems = []
 
 getData()
 
+filter.addEventListener('input', (e) => filterData(e.target.value))
+
 async function getData() {
     const res = await fetch('https://randomuser.me/api?results=50')
 
     const { results } = await res.json()
 
     // Clear results
-    results.innerHTML = ''
+    result.innerHTML = ''
 
     results.forEach(user => {
         const li = document.createElement('li')
@@ -21,9 +23,19 @@ async function getData() {
             <img src="${user.picture.large}" alt="${user.name.first}">
             <div class="user-info">
                 <h4>${user.name.first} ${user.name.last}</h4>
-                <p>${user.name.city}, ${user.location.country}</p>
+                <p>${user.location.city}, ${user.location.country}</p>
             </div>
         `
         result.appendChild(li)
+    })
+}
+
+function filterData(searchTerm) {
+    listItems.forEach(item => {
+        if(item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+            item.classList.remove('hide')
+        } else {
+            item.classList.add('hide')
+        }
     })
 }
