7a6e376f15d59a8176eae53c9c3e430728c967d7
steve_D <scd346@nyu.edu>
2021-06-15 15:15:41 -0400

    score fn added

--- Day46/script.js
+++ Day46/script.js
@@ -79,5 +79,21 @@ function getSelected() {
 
 submitBtn.addEventListener('click', () => {
     const answer = getSelected()
-    console.log(answer)
+    if(answer) {
+        if(answer === quizData[currentQuiz].correct) {
+            score++
+        }
+
+        currentQuiz++
+
+        if(currentQuiz < quizData.length) {
+            loadQuiz()
+        } else{
+            quiz.innerHTML = `
+            <h2>You answered ${score}/${quizData.length} questions correctly.</h2>
+
+            <button onclick="location.reload()">Reload</button>
+            `
+        }
+    }
 })
