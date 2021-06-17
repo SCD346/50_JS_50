343e60c51d2ba7690d2cf0bd56af0ed9e25bdc37
steve_D <scd346@nyu.edu>
2021-06-17 18:20:36 -0400

    writes to local storage

--- Day49/script.js
+++ Day49/script.js
@@ -2,6 +2,12 @@ const form = document.getElementById('form')
 const input = document.getElementById('input')
 const todosUL = document.getElementById('todos')
 
+const todos = JSON.parse(localStorage.getItem('todos'))
+
+if(todos) {
+    todos.forEach(todo => addTodo(todo))
+}
+
 form.addEventListener('submit', (e) => {
     e.preventDefault()
 
@@ -18,18 +24,38 @@ function addTodo(todo) {
         if(todo && todo.completed){
             todoEl.classList.add('completed')
         }
+
         todoEl.innerText = todoText
 
         todoEl.addEventListener('click', () => todoEl.classList.toggle('completed'))
+        updateLS()
 
         todoEl.addEventListener('contextmenu', () => {
             e.preventDefault()
 
             todoEl.remove()
+            updateLS()
         })
 
         todosUL.appendChild(todoEl)
 
         input.value = ''
+
+        updateLS()
     }
+}
+
+function updateLS() {
+    todosEL = document.querySelectorAll('li')
+
+    const todos = []
+
+    todosEL.forEach(todoEl => {
+        todos.push({
+            text: todoEl.innerText,
+            completed: todoEl.classList.contains('completed')
+        })
+    })
+
+    localStorage.setItem('todos', JSON.stringify(todos))
 }
