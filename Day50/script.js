c2875185c6710dfad64d5d08934d88db1541db51
steve_D <scd346@nyu.edu>
2021-06-28 10:16:16 -0500

    selectors, button to start game

--- Day50/script.js
+++ Day50/script.js
@@ -1,3 +1,12 @@
 const screens = document.querySelectorAll('.screen')
+const choose_insect_btns = document.querySelectorAll('.choose-insect-btn')
+const start_btn = document.getElementById('start-btn')
+const game_container = document.getElementById('game-container')
+const timeEl = document.getElementById('time')
+const scoreEl = document.getElementById('score')
+const messages = document.getElementById('message')
+let seconds = 0
+let score = 0
+let selected_insect = {}
 
-// screens[0].classList.add('up')
+start_btn.addEventListener('click', () => screens[0].classList.add('up'))
